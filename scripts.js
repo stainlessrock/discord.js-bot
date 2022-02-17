13452f4a7454c3f133f82883fa0ebdef08b49a6e
stainless77steel <99351065+stainless77steel@users.noreply.github.com>
2022-02-17 20:22:39 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: 32767 });
+
+client.on("ready", async () => {
+	
+  console.log("Welcome to earth Bot!");
+
+
+
+});
+client.on("messageCreate", async (message) => {
+
+  if (message.content.startsWith("hello"))
+
+
+    message.reply("hello to you too");
+
+
+});
+client.login("BOT_TOKEN");
